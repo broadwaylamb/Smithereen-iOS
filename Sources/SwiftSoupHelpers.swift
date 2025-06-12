broadwaylamb/Smithereen-iOS

commit 9a84d34ed6da81da331a4b9d6fec20ac794a07ed

Implement a better HTML visitor

Sources/SwiftSoupHelpers.swift
@@ -1,0 +1,38 @@
+import SwiftSoup
+
+/// Like `SwiftSoup.NodeVisitor` but with typed throws for better type safety
+protocol NodeVisitor {
+    associatedtype Error: Swift.Error
+
+    func head(_ node: Node, _ depth: Int) throws(Error)
+
+    func tail(_ node: Node, _ depth: Int) throws(Error)
+}
+
+extension Node {
+    func traverse<Visitor: NodeVisitor>(_ visitor: Visitor) throws(Visitor.Error) {
+        var node: Node? = self
+        var depth: Int = 0
+
+        while let currentNode = node {
+            try visitor.head(currentNode, depth)
+            if currentNode.hasChildNodes() {
+                node = currentNode.childNode(0)
+                depth += 1
+            } else {
+                while !currentNode.hasNextSibling() && depth > 0 {
+                    let parent = currentNode.parent()
+                    try visitor.tail(currentNode, depth)
+                    node = parent
+                    depth -= 1
+                }
+                let nextSib = currentNode.nextSibling()
+                try visitor.tail(currentNode, depth)
+                if node === self {
+                    break
+                }
+                node = nextSib
+            }
+        }
+    }
+}

Sources/HTML+AttributedString.swift
@@ -4,10 +4,7 @@ import SwiftSoup
 
 func renderHTML(_ html: Element) -> AttributedString {
     let renderer = HtmlRenderer()
-
-    // The renderer itself doesn't throw, so it's safe to force-try.
-    try! html.traverse(renderer)
-
+    html.traverse(renderer)
     return renderer.result
 }
 
@@ -23,6 +20,7 @@ extension String {
 }
 
 private class HtmlRenderer: NodeVisitor {
+    typealias Error = Never
     var result = AttributedString()
     private var attributesStack: [AttributeContainer] = [AttributeContainer().font(.body)]
     private var presentationIntentCounter = 1
@@ -31,7 +29,7 @@ private class HtmlRenderer: NodeVisitor {
     private var lastWasWhite = false
     private var stripLeading = true
 
-    func head(_ node: Node, _ depth: Int) throws {
+    func head(_ node: Node, _ depth: Int) {
         var newContainer = attributesStack.last ?? AttributeContainer()
         if let textNode = node as? TextNode {
             let text = textNode.getWholeText()
@@ -42,7 +40,7 @@ private class HtmlRenderer: NodeVisitor {
             // If it sends other tags, we ignore them.
             switch element.tagNameNormal() {
             case "a":
-                let href = try element.attr("href")
+                let href = (try? element.attr("href")) ?? ""
                 if !href.isEmpty {
                     newContainer.link = URL(string: href)
                 }
@@ -87,7 +85,7 @@ private class HtmlRenderer: NodeVisitor {
         attributesStack.append(newContainer)
     }
 
-    func tail(_ node: Node, _ depth: Int) throws {
+    func tail(_ node: Node, _ depth: Int) {
         attributesStack.removeLast()
         if let element = node as? Element, element.tagNameNormal() == "pre" {
             codeBlockDepth -= 1

commit 88e90a0a9f93421137afdcad220c74f046a5f305

Fix a regression in HTML rendering
that was caused by 9a84d34ed6da81da331a4b9d6fec20ac794a07ed

Sources/SwiftSoupHelpers.swift
@@ -20,18 +20,22 @@ extension Node {
                 node = currentNode.childNode(0)
                 depth += 1
             } else {
-                while !currentNode.hasNextSibling() && depth > 0 {
+                var node2: Node? = currentNode
+                while let currentNode = node2,
+                      !currentNode.hasNextSibling() && depth > 0 {
                     let parent = currentNode.parent()
                     try visitor.tail(currentNode, depth)
-                    node = parent
+                    node2 = parent
                     depth -= 1
                 }
-                let nextSib = currentNode.nextSibling()
-                try visitor.tail(currentNode, depth)
-                if node === self {
-                    break
+                if let currentNode = node2 {
+                    let nextSib = currentNode.nextSibling()
+                    try visitor.tail(currentNode, depth)
+                    if currentNode === self {
+                        break
+                    }
+                    node = nextSib
                 }
-                node = nextSib
             }
         }
     }