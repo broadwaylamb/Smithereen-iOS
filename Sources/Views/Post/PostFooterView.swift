88e85a8331d356a602d3929196081aa27ff69f33
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2025-06-26 12:16:22 +0200

    Haptic feedback when tapping the like button

--- Sources/Views/Post/PostFooterView.swift
+++ Sources/Views/Post/PostFooterView.swift
@@ -166,6 +166,7 @@ private struct LikeButton: View {
             }
             .buttonStyle(.borderless)
             .tint(palette.regularPostLikeAndRepostButton)
+            .likeButtonFeedback(liked: liked)
         } else {
             CompactPostFooterButton(
                 alignment: .center,
@@ -176,6 +177,33 @@ private struct LikeButton: View {
                 highlighted: liked,
                 action: action,
             )
+            .likeButtonFeedback(liked: liked)
+        }
+    }
+}
+
+@MainActor
+private let impactFBG = UIImpactFeedbackGenerator()
+
+@MainActor
+private let notificationFBG = UINotificationFeedbackGenerator()
+
+private extension View {
+
+    func likeButtonFeedback(liked: Bool) -> some View {
+        if #available(iOS 17.0, *) {
+            // NOTE: The condition here is inverted because SwiftUI.
+            // We want .success when the user likes a post
+            // and .impact when the user withdraws their like.
+            // Counterintuitive as it is, this condition is correct.
+            return sensoryFeedback(liked ? .impact : .success, trigger: liked)
+        }
+        return onChangePolyfill(of: liked) {
+            if liked {
+                notificationFBG.notificationOccurred(.success)
+            } else {
+                impactFBG.impactOccurred()
+            }
         }
     }
 }
